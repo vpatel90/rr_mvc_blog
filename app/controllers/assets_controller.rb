792d1b16407ae45e54368c41515d4f5a589cc51a
Justin Herrick <justin@justinherrick.com>
2016-03-24 12:58:37 -0500

    Adding the ability to autocompile scss files per request.

--- app/controllers/assets_controller.rb
+++ app/controllers/assets_controller.rb
@@ -1,6 +1,11 @@
 class AssetsController < ApplicationController
   def handle
-    data = File.read("app/assets/#{params[:type]}/#{params[:name]}")
+    file_name = "app/assets/#{params[:type]}/#{params[:name]}"
+    if request[:format] == "css" && File.exist?(file_name.gsub('css', 'scss'))
+      data = `sass #{file_name.gsub('css', 'scss')}`
+    else
+      data = File.read(file_name)
+    end
     format_type = WEBrick::HTTPUtils::DefaultMimeTypes[request[:format]]
     render data, as: format_type
   end
