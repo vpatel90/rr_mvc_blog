4e2e6356be93826a3d28114a3b9251999a6de3ec
Vivek Patel <vnp229@gmail.com>
2016-03-24 21:35:32 -0500

    commiting before pull

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,15 @@
 class PostsController < ApplicationController
   def index
     @all_posts = get_all_posts
+    @total = (@all_posts.size / 10) + 1
+    if params[:limit].nil? || params[:limit] == 0
+      @rendered_posts = @all_posts[0,10]
+    else
+      @rendered_posts = @all_posts[params[:limit],10]
+    end
+    binding.pry
     render_template 'blog/index.html.erb'
+
   end
 
   def show
